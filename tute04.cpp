efeea3c4ab79fdc2ac4cbce03fc5e3ff92ff42ba
it21081798 <87408026+it21081798@users.noreply.github.com>
2021-07-24 12:11:30 +0000

    Question 1,2,3,4

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,22 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+long fact =1;
+for(int i=no; i>=1; i--)
+{
+  fact=fact*i;
+}
+return fact;
+}
+long nCr(int n,int r)
+{
+  return Factorial(n)/(Factorial(r)* Factorial(n-r));
+}
+
+
+
+
+
+
